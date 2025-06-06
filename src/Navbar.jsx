7c0352cf39dbaf5a8f12452a7ba73c3139e94ed1
Abuzar hasan himu <abuzarhasanhimu0@gmail.com>
2025-06-06 23:37:27 +0800

    setup

---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='text-6xl btn btn-accent'>
+            Nav
+        </div>
+    );
+};
+
+export default Navbar;
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from './Navbar';
 
 const Root = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+<Navbar></Navbar>
+<Outlet></Outlet>
+
+
+        </>
     );
 };
 
