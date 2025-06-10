2126c947d1cc6170b7dc2818121c5da9ca3989b9
Abuzar hasan himu <abuzarhasanhimu0@gmail.com>
2025-06-10 11:22:05 +0600

    navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,8 +2,48 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className='text-6xl btn btn-accent'>
-            Nav
+        <div className='w-11/12 mx-auto py-5'>
+
+            <div className="navbar justify-between  bg-base-100 shadow-2xl">
+
+                <div >
+
+                    <a className="btn hover:bg-amber-200 text-xl"><span className='text-2xl font-bold text-sky-400'><i>Hero</i></span>Restaurant</a>
+                </div>
+
+<div >
+<a className='text-lg p-2 bg-gray-100 hover:bg-amber-200' href="">Home</a>
+<a className='mx-2 text-lg p-2 bg-gray-100 hover:bg-amber-200' href="">All Food</a>
+<a className='text-lg p-2 bg-gray-100 hover:bg-amber-200 ' href="">Gallery</a>
+
+
+</div>
+
+                <div >
+                    {/* IMAGE */}
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                            </div>
+                       
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            
+                            <li><a className='text-xl'>Add Food</a></li>
+                            <li><a className='text-xl'>My Food</a></li>
+                            <li><a className='text-xl'>My Orders</a></li>
+                            <li><a className='text-xl'>Log Out</a></li>
+                        </ul>
+                    </div>
+                </div>
+              
+            </div>
+              
         </div>
     );
 };
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Root = () => {
     return (
         <>
-<Navbar></Navbar>
+<div className='bg-gray-300'> 
+    <Navbar></Navbar>
 <Outlet></Outlet>
+<Footer></Footer>
+</div>
 
 
         </>
