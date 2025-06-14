d4b113cb960d656afe098c8dd58b331d9d7d600f
Abuzar hasan himu <abuzarhasanhimu0@gmail.com>
2025-06-14 15:44:09 +0800

    theme

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Root = () => {
+    const [theme, setTheme] = useState('light');
+
+
+  // Load theme 
+ useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    setTheme(savedTheme);
+    document.documentElement.setAttribute("data-theme", savedTheme);
+  }, []);
+
+
+   // Update theme 
+ useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(prev => (prev === "light" ? "dark" : "light"));
+  };
+
     return (
         <>
+         <div className=' w-0.5 mx-auto'>
+          <button onClick={toggleTheme} className="btn btn-secondary w-8 h-8">
+           <input type="checkbox" defaultChecked className="toggle toggle-success w-5 h-5" /> 
+        </button>
+        </div>
+
+
+        {/* Content */}
 <div > 
     <Navbar></Navbar>
 <Outlet></Outlet>
