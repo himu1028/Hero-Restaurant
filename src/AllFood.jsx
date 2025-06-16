1ef625546b2fec4be865eb81657a997404d7e38d
Abuzar hasan himu <abuzarhasanhimu0@gmail.com>
2025-06-16 11:40:22 +0600

    last

--- src/AllFood.jsx
+++ src/AllFood.jsx
@@ -22,7 +22,53 @@ const AllFood = () => {
   return (
     <>
     <div    >
+      {/* <div 
+       
+      className="w-11/12 mx-auto mt-6 flex justify-center"
+
+      >
+        <label className="input w-full max-w-md flex items-center gap-2 bg-white shadow px-4 py-2 rounded-md">
+          <svg
+            className="h-5 w-5 opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input
+            type="search"
+            onChange={(e) => setSearch(e.target.value)}
+            required
+            placeholder="Search Food"
+            className="w-full outline-none"
+          />
+        </label>
+      </div> */}
+
       <div 
+         style={{ backgroundImage: `url(${imageUrl})`}}
+      className=" w-11/12 mx-auto mt-6 rounded-lg">
+        <div className="text-3xl md:text-5xl pt-10 my-4 text-center text-purple-700 font-bold">
+          <Typewriter
+            words={['All Food', 'Welcome to All Food!', 'All Food']}
+            loop={5}
+            cursor
+            cursorStyle="|"
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </div>
+         <div 
        
       className="w-11/12 mx-auto mt-6 flex justify-center"
 
@@ -53,21 +99,6 @@ const AllFood = () => {
           />
         </label>
       </div>
-
-      <div 
-         style={{ backgroundImage: `url(${imageUrl})`}}
-      className=" w-11/12 mx-auto mt-6 rounded-lg">
-        <div className="text-3xl md:text-5xl pt-10 my-4 text-center text-purple-700 font-bold">
-          <Typewriter
-            words={['All Food', 'Welcome to All Food!', 'All Food']}
-            loop={5}
-            cursor
-            cursorStyle="|"
-            typeSpeed={70}
-            deleteSpeed={50}
-            delaySpeed={1000}
-          />
-        </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-4 py-5">
           {all.map((topCard) => (
